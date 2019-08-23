d201a998a2b43840b773ce4bb919fd316383588c
Joseph Francis <josephfrancis100@gmail.com>
2019-08-23 01:20:24 -0500

    Added layers control panel to webmaps

--- map.py
+++ map.py
@@ -3,7 +3,8 @@ import pandas
 
 map_val = folium.Map(location=[29.548428, -95.522724])  # tiles="Stamen Terrain", zoom_start=2
 
-fg = folium.FeatureGroup(name="My Map")
+fg = folium.FeatureGroup(name="Volcanoes")
+fg_population = folium.FeatureGroup("Population")
 
 
 def color_producer(elevation):
@@ -24,7 +25,8 @@ for (lat, lng, elv) in zip(lat, lng, elev):
     fg.add_child(
         folium.Marker(location=[lat, lng], popup=elv, icon=folium.Icon(color=color_producer(elv))))
 
-fg.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(), style_function=lambda x: {"fillColor": "blue" if x["properties"]["POP2005"] < 10000000 else "orange" if 10000000 <= x["properties"]["POP2005"] < 20000000 else "red"}))
+fg_population.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(), style_function=lambda x: {"fillColor": "blue" if x["properties"]["POP2005"] < 10000000 else "orange" if 10000000 <= x["properties"]["POP2005"] < 20000000 else "red"}))
 map_val.add_child(fg)
-
+map_val.add_child(fg_population)
+map_val.add_child(folium.LayerControl())
 map_val.save("Map1.html")
